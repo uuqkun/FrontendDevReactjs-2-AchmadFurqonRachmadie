8ba19707b6a9aa21b97395219792a746f5214848
Achmad Furqon Rachmadie <achmadfurqonrachmadie@gmail.com>
2023-11-16 08:04:22 +0700

    add categories samples

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const sampleCategories = [
+  "all",
+  "Lebanese",
+  "Mediterranean",
+  "Middle Eastern",
+  "Italian",
+  "Pizza",
+  "Cafe",
+  "Chinese",
+  "Indian",
+  "Bar",
+  "Asian",
+  "International",
+];
